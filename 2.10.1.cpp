756cc5fc91bace51de5a16e718a95b0ae5210d23
gerasimovMP <gerasmaks@yandex.ru>
2016-11-17 09:51:39 +0300

    Лаб. р. 2.10.1

---
+++ 2.10.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(void)
+{
+
+int l;
+cout<<"Diagonal"<<endl;
+cin>>l;
+cout<<"Perimetr:"<<2*sqrt(2.0)*l<<endl<<"Square':"<<(l*l)/2<<endl;
+
+system("pause");
+return 1;
+}
