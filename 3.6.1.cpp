fbc979a34730c8d5f8f3e238040f928285893cb3
gerasimovMP <gerasmaks@yandex.ru>
2016-11-21 12:09:56 +0300

    Лаб. р. 3.6.1

---
+++ 3.6.1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(void)
+{
+    int y,x;
+    cout<<"x = "<<endl;
+    cin>>x;
+    if (((x>=-898888888676)&&(x<(-3)))||(((x>3)&&(x<=(99999999999)))))
+    {
+            y=3;
+    }
+    else
+if
+        ((x>=(-3))&(x<=0))
+   {
+    y=(x+3);
+}
+    else
+
+{
+        y=(3-x);
+    }
+
+            cout<<"y = "<<y<<endl;
+
+
+
+
+
+    system("pause");
+    return 1;
+}
+
